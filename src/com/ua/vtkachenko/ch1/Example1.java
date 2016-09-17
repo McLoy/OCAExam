3522ec08b061a8a2d9e22cac1d4954e8c96c051c
McLoy <tkachenko.vlad@hotmail.com>
2016-09-18 00:46:52 +0300

    Initial commit

---
+++ src/com/ua/vtkachenko/ch1/Example1.java
@@ -0,0 +1,9 @@
+package com.ua.vtkachenko.ch1;
+
+class Example1 {
+    private String name;
+}
+
+class Ex2{
+
+}
---
+++ src/com/ua/vtkachenko/ch2/Example2.java
@@ -0,0 +1,7 @@
+package com.ua.vtkachenko.ch2;
+
+public class Example2 {
+    public static void main(String[] a) {
+        System.out.println("1");
+    }
+}
