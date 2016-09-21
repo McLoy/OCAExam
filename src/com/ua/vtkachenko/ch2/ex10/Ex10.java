44ff31507a377157f1cbd34e32a50d6919e0ac28
McLoy <tkachenko.vlad@hotmail.com>
2016-09-21 19:00:41 +0300

    update

---
+++ src/com/ua/vtkachenko/ch2/ex10/Ex10.java
@@ -0,0 +1,10 @@
+package com.ua.vtkachenko.ch2.ex10;
+
+public class Ex10 {
+    public static void main(String[] args) {
+        byte a = 40, b = 50;
+        //byte sum = (byte) a + b;
+        byte sum = (byte) (a + b);
+        System.out.println(sum);
+    }
+}
---
+++ src/com/ua/vtkachenko/ch2/ex9/Ex9.java
@@ -0,0 +1,10 @@
+package com.ua.vtkachenko.ch2.ex9;
+
+public class Ex9 {
+    public static void main(String[] args) {
+        for (int i = 0; i < 10; ) {
+            i = ++i;
+            System.out.println("Hello World");
+        }
+    }
+}
