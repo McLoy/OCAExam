9e8d34ac8bcb00e7940e51550f659633c4e81823
McLoy <tkachenko.vlad@hotmail.com>
2016-09-21 18:52:48 +0300

    update

---
+++ src/com/ua/vtkachenko/ch2/ex7/Ex7.java
@@ -0,0 +1,10 @@
+package com.ua.vtkachenko.ch2.ex7;
+
+public class Ex7 {
+    public static void main(String[] args) {
+
+        int x = 5;
+        System.out.println(x >2 ? x < 4 ? 10 : 8 : 7);
+
+    }
+}
