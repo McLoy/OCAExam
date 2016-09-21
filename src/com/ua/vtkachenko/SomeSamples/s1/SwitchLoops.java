54a15bec2cbaba14411f6724001921158285d60b
McLoy <tkachenko.vlad@hotmail.com>
2016-09-21 21:29:54 +0300

    update

---
+++ src/com/ua/vtkachenko/SomeSamples/s1/SwitchLoops.java
@@ -0,0 +1,18 @@
+package com.ua.vtkachenko.SomeSamples.s1;
+
+public class SwitchLoops {
+    public static void main(String[] args) {
+
+        int k = 10;
+        boolean tt = true;
+
+        switch (k) {
+            case 0:
+                System.out.println("0");
+                break;
+            case 10:
+                System.out.println("10");
+                break;
+        }
+    }
+}
---
+++ src/com/ua/vtkachenko/ch2/ex3/CompareValues.java
@@ -0,0 +1,14 @@
+package com.ua.vtkachenko.ch2.ex3;
+
+public class CompareValues {
+    public static void main(String[] args) {
+
+        int x = 0;
+
+        while (x++ <10){
+        }
+        //String message = x > 10 ? "Greater than" : false;
+        System.out.println(x);
+
+    }
+}
