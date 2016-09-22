c7014a83660a378bc0782e7dc4e627f68bbc771e
McLoy <tkachenko.vlad@hotmail.com>
2016-09-22 23:28:41 +0300

    update

---
+++ src/com/ua/vtkachenko/SomeSamples/s4/StringObjects.java
@@ -0,0 +1,12 @@
+package com.ua.vtkachenko.SomeSamples.s4;
+
+public class StringObjects {
+    public static void main(String[] args) {
+
+        String alpha = "";
+        for (char current = 'a';  current <= 'z' ; current++) {
+            alpha += current;
+            System.out.println(alpha);
+        }
+    }
+}
