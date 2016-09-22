905f09283c77899c615f0431d0b9dfc522f9b8f5
McLoy <tkachenko.vlad@hotmail.com>
2016-09-22 23:31:07 +0300

    update

--- src/com/ua/vtkachenko/SomeSamples/s4/StringObjects.java
+++ src/com/ua/vtkachenko/SomeSamples/s4/StringObjects.java
@@ -4,9 +4,10 @@ public class StringObjects {
     public static void main(String[] args) {
 
         String alpha = "";
-        for (char current = 'a';  current <= 'z' ; current++) {
+        for (char current = 'a';  current <= 'z' ; current++)
             alpha += current;
             System.out.println(alpha);
-        }
+
+
     }
 }
