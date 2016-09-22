1b55edb3e148b2bf484fd4313ec3941bcb7d2541
McLoy <tkachenko.vlad@hotmail.com>
2016-09-22 22:48:52 +0300

    update

---
+++ src/com/ua/vtkachenko/SomeSamples/s2/Ch3.java
@@ -0,0 +1,11 @@
+package com.ua.vtkachenko.SomeSamples.s2;
+
+public class Ch3 {
+    public static void main(String[] args) {
+        //System.out.println("b" + 1 +2);
+        String s1 = "1";
+        String s2 = s1.concat("2");
+        s2.concat("3");
+        System.out.println(s2);
+    }
+}
