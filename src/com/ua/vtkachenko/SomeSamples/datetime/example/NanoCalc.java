06aa610ba22b144e446026a1931ddea5c4853ae0
McLoy <tkachenko.vlad@hotmail.com>
2016-09-26 23:48:07 +0300

    update

--- src/com/ua/vtkachenko/SomeSamples/datetime/example/NanoCalc.java
+++ src/com/ua/vtkachenko/SomeSamples/datetime/example/NanoCalc.java
@@ -8,15 +8,6 @@ public class NanoCalc {
     public static void main(String[] args) {
 
         LocalTime time = LocalTime.of(22,00,30,0);
-        int count = 0;
-        List<LocalTime> arr = new ArrayList<>();
-        arr.add(time);
-        while (arr.get(count) != LocalTime.of(22,00,31,0)){ //fatality loop - dont start it!!!
-            LocalTime a = time.plusNanos(100000000);
-            arr.add(a);
-            count += 1;
-            System.out.println(a);
-        }
-        System.out.println(count*100000000 + " nanos" + " in one second of time");
+
     }
 }
