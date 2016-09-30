ab5ff383fb0c7814da168a2326cba4db3026023a
McLoy <tkachenko.vlad@hotmail.com>
2016-09-30 23:31:21 +0300

    update

---
+++ src/com/ua/vtkachenko/ch4/ex20/BirdSeed.java
@@ -0,0 +1,28 @@
+package com.ua.vtkachenko.ch4.ex20;
+
+public class BirdSeed {
+
+    private int numberBags;
+    boolean call;
+
+    public BirdSeed(){
+        //BirdSeed(2);
+        //new BirdSeed(2);
+        this(2);
+        //LINE1
+        call = false;
+        //LINE2
+        //BirdSeed(2);
+        //new BirdSeed(2);
+        //this(2);
+    }
+
+    public BirdSeed(int numberBags){
+        this.numberBags = numberBags;
+    }
+
+    public static void main(String[] args) {
+        BirdSeed seed = new BirdSeed();
+        System.out.println(seed.numberBags);
+    }
+}
