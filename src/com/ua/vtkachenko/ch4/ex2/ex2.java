a9bcfd92f4797f840c4373d9217a7beaac06fda9
McLoy <tkachenko.vlad@hotmail.com>
2016-09-30 21:14:57 +0300

    update

---
+++ src/com/ua/vtkachenko/ch4/ex2/ex2.java
@@ -0,0 +1,13 @@
+package com.ua.vtkachenko.ch4.ex2;
+
+public class ex2 {
+
+    final static void method4(){}
+    //public final int void method(){]}
+    //private void int method(){}
+    static final void method3(){}
+    //void final method(){}
+    //void public method(){}
+
+
+}
---
+++ src/com/ua/vtkachenko/ch4/ex4/ex4.java
@@ -0,0 +1,4 @@
+package com.ua.vtkachenko.ch4.ex4;
+
+public class ex4 {
+}
