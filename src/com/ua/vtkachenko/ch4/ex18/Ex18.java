270507b279281d3e3f71baf406edb5b8dbb07054
McLoy <tkachenko.vlad@hotmail.com>
2016-10-03 13:56:03 +0300

    update

---
+++ src/com/ua/vtkachenko/ch4/ex18/Ex18.java
@@ -0,0 +1,21 @@
+package com.ua.vtkachenko.ch4.ex18;
+
+import java.util.Objects;
+
+public class Ex18 {
+
+    private String name;
+    public int count = 0;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
