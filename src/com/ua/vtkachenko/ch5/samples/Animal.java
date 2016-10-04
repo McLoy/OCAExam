258a1ac12984da291d3b907e25d74e20752c8ad6
McLoy <tkachenko.vlad@hotmail.com>
2016-10-04 14:11:53 +0300

    update

---
+++ src/com/ua/vtkachenko/ch5/samples/Animal.java
@@ -0,0 +1,15 @@
+package com.ua.vtkachenko.ch5.samples;
+
+public class Animal {
+
+    private int age;
+
+//    public Animal(){
+//        this.age = 5;
+//    }
+
+    public Animal(int age){
+        this.age = age;
+    }
+
+}
---
+++ src/com/ua/vtkachenko/ch5/samples/Lion.java
@@ -0,0 +1,15 @@
+package com.ua.vtkachenko.ch5.samples;
+
+public class Lion extends Animal{
+
+    private int lionAge;
+
+    public Lion(){
+        super(10);
+        this.lionAge = 2;
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
