0d54af9a406eb4bf904337f1fc7ff47fcb6b5a9c
McLoy <tkachenko.vlad@hotmail.com>
2016-10-04 15:04:44 +0300

    update

--- src/com/ua/vtkachenko/ch5/samples/Shark.java
+++ src/com/ua/vtkachenko/ch5/samples/Shark.java
@@ -15,8 +15,24 @@ public class Shark extends Fish {
         System.out.print(" with " + numberOfFins + " fins");
     }
 
+    public void displaySharkDetailsByThis(){
+        System.out.print("Shark with age:" + this.getAge());
+        System.out.print(" and " + this.size + " meters long");
+        System.out.print(" with " + this.numberOfFins + " fins");
+    }
+
+    public void displaySharkDetailsBySuper(){
+        System.out.print("Shark with age:" + super.getAge());
+        System.out.print(" and " + super.size + " meters long");
+        System.out.print(" with " + numberOfFins + " fins");
+    }
+
     public static void main(String[] args) {
         Shark s = new Shark(7);
         s.displaySharkDetails();
+        System.out.println();
+        s.displaySharkDetailsByThis();
+        System.out.println();
+        s.displaySharkDetailsBySuper();
     }
 }
