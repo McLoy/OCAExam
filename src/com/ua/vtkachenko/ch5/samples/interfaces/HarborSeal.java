9bb5e5f0a01192aef27a4e014cbc536b94e0a4c5
McLoy <tkachenko.vlad@hotmail.com>
2016-10-07 00:50:35 +0300

    update

---
+++ src/com/ua/vtkachenko/ch5/samples/interfaces/HarborSeal.java
@@ -0,0 +1,4 @@
+package com.ua.vtkachenko.ch5.samples.interfaces;
+
+public abstract class HarborSeal  implements HasTail, HashWhiskers{
+}
---
+++ src/com/ua/vtkachenko/ch5/samples/interfaces/HashWhiskers.java
@@ -0,0 +1,6 @@
+package com.ua.vtkachenko.ch5.samples.interfaces;
+
+public interface HashWhiskers {
+
+    public int getNumberOfWhiskers();
+}
